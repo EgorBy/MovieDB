1cef85967e4b8f37c2b7671112715842b7161f27
Egor <pixer.ru@mail.ru>
2023-02-21 15:48:14 +0300

    movieDB first js

--- js/script.js
+++ js/script.js
@@ -14,13 +14,41 @@
 
 'use strict';
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против..."
+  ]
 };
+
+adv.forEach(item => {
+  item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = "url('img/bg.jpg')";
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+  movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1}. ${film}
+      <div class="delete"></div>
+    </li>  
+  `;
+});
+
+
+
 
